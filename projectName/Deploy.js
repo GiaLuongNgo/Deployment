39937e830f47d870d8084a60381f9421139a0c7f
GiaLuongNgo <luong.ngogia1997@gmail.com>
2019-06-19 14:04:34 -0400

    Call the functions and Implement an event listener for  contract

--- projectName/Deploy.js
+++ projectName/Deploy.js
@@ -2,20 +2,41 @@ const ethers = require('ethers');
 require('dotenv').config();
 
 const contractInfo = require('./build/contracts/SimpleStorage.json');
-const bytecode = contractInfo.code;
+// const bytecode = contractInfo.code;
 const ABI = contractInfo.abiDefinition;
 
 let provider = ethers.getDefaultProvider('ropsten');
 
-console.log(process.env.PK);
-let wallet = new ethers.Wallet(process.env.PK, provider);
+console.log(process.env.pk);
+let wallet = new ethers.Wallet(process.env.pk, provider);
+// let ContractFactory = new ethers.ContractFactory(ABI, bytecode, wallet);
+// async function deployContract() {
+//   let contract = await ContractFactory.deploy();
+//   await contract.deployed();
+//   console.log(contract);
+// }
 
-let ContractFactory = new ethers.ContractFactory(ABI, bytecode, wallet);
+// deployContract();
+let contractAddress ="0x86B7746677A1F652abA4379ee7c74eDBe32635E2" ;
+let readOnlyContract = new ethers.Contract(contractAddress, ABI, provider);
+let readAndWriteContract = readOnlyContract.connect(wallet);
 
-async function deployContract() {
-  let contract = await ContractFactory.deploy();
-  await contract.deployed();
-  console.log(contract);
+//call function
+async function getValue() {
+  let value = await readAndWriteContract.get();
+  console.log("get value:", value.toNumber());
 }
+getValue();
 
-deployContract();
+async function set() {
+  let tx = await readAndWriteContract.set(9);
+  console.log(tx)
+}
+set();
+
+//event listener
+
+readOnlyContract.on('Data', (x) => {
+  console.log(x.toNumber())
+})
+
--- projectName/contracts/SimpleStorage.sol
+++ projectName/contracts/SimpleStorage.sol
@@ -1,18 +1,16 @@
 pragma solidity ^0.5.0;
 //Declares a new contract
 contract SimpleStorage {
+    event Data (uint data);
     uint storedData;
     address owner;
     constructor() public {
         owner = msg.sender;
     }
-    modifier onlyOwner() {
-        require(owner==msg.sender,"not Owner");
-        _;
-    }
 
-    function set(uint x) public onlyOwner {
+    function set(uint x) public  {
         storedData = x;
+        emit Data(storedData);
     }
 
     function get() public view returns (uint) {
